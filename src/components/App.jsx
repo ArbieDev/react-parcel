5ccfcba33d98fb7b742ba30fbba1d0151cf5536d
Arbie C <acskaleidoscope@gmail.com>
2018-02-17 00:37:49 +0900

    import axios for reqests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      character: {},
+    };
+  }
+
+  pickMeACharacter() {
+    const from = 1;
+    const to = 88;
+    const num = Math.floor(Math.random() * to) + from;
+    this.fetchCharacterWithId(num);
+  }
+
+  fetchCharacterWithId(id) {
+    this.setState({ isLoading: true });
+    const url = `https://swapi.co/api/people/${id}/`;
+    axios.get(url).then(response => {
+      this.setState({ character: response.data, isLoading: false });
+    });
+  }
+
   render() {
     return (
-      <div>LOADED!</div>
+      <div>
+        <div className="container">
+          <div className="">
+            <Button
+              bsStyle="warning"
+              bsSize="small"
+              disabled={this.state.isLoading}
+              onClick={() => this.pickMeACharacter()}
+            >
+              WHO AM I?
+            </Button>
+          </div>
+          <div>{this.state.character.name}</div>
+        </div>
+      </div>
     );
   }
 }
